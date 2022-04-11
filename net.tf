965c0a557fbfc1cbe74ac1a0ed75589482cb20fd
Stevie Stringer <sstringer@gentlemanfox.co.uk>
2022-04-11 15:45:43 +0100

    forgot to run terraform fmt

--- net.tf
+++ net.tf
@@ -1,27 +1,27 @@
 resource "azurerm_network_security_group" "nsg" {
   name                = local.nsgName
-  location                    = var.location
-  resource_group_name         = var.rg
+  location            = var.location
+  resource_group_name = var.rg
 
   dynamic "security_rule" {
-      for_each = var.sec_rules
-      content {
-        name                       = security_rule.value["name"]
-        priority                   = security_rule.value["priority"]
-        direction                  = security_rule.value["direction"]
-        access                     = security_rule.value["access"]
-        protocol                   = security_rule.value["protocol"]
-        source_port_range          = security_rule.value["source_port_range"]
-        destination_port_range     = security_rule.value["destination_port_range"]
-        source_address_prefix      = security_rule.value["source_address_prefix"]
-        destination_address_prefix = security_rule.value["destination_address_prefix"]
-      }
+    for_each = var.sec_rules
+    content {
+      name                       = security_rule.value["name"]
+      priority                   = security_rule.value["priority"]
+      direction                  = security_rule.value["direction"]
+      access                     = security_rule.value["access"]
+      protocol                   = security_rule.value["protocol"]
+      source_port_range          = security_rule.value["source_port_range"]
+      destination_port_range     = security_rule.value["destination_port_range"]
+      source_address_prefix      = security_rule.value["source_address_prefix"]
+      destination_address_prefix = security_rule.value["destination_address_prefix"]
+    }
   }
 }
 
 #Bind each NIC to the NSG
 resource "azurerm_network_interface_security_group_association" "ngs_nic" {
-    count = var.vmCount
-    network_interface_id      = element(azurerm_network_interface.nic.*.id,count.index)
-    network_security_group_id = azurerm_network_security_group.nsg.id
+  count                     = var.vmCount
+  network_interface_id      = element(azurerm_network_interface.nic.*.id, count.index)
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
--- variables.tf
+++ variables.tf
@@ -1,59 +1,59 @@
 variable "projectName" {
-    description = "Naming prefix for the Azure resources"
-    type = string
-    default = "testweblb-proj"
+  description = "Naming prefix for the Azure resources"
+  type        = string
+  default     = "testweblb-proj"
 }
 
 #Required!
 variable "rg" {
-    description = "The name of the resource group in which the resources will be built"
-    type = string
+  description = "The name of the resource group in which the resources will be built"
+  type        = string
 }
 
 #Required!
 variable "location" {
-    description = "The Azure region where the will be built"
-    type = string
+  description = "The Azure region where the will be built"
+  type        = string
 }
 
 #Required!
 variable "subnetId" {
-    description = "The subnet ID of the subnet that the resources will attach to"
-    type = string
+  description = "The subnet ID of the subnet that the resources will attach to"
+  type        = string
 }
 
 #Required!
 variable "cloudInitPath" {
-    description = "The path to the required cloud-init.yaml file for example './cloud-init.yaml'.  It is recommended to download and use https://github.com/mr-stringer/testwebserver/releases/download/v1.0.0/cloud-init.yaml"
-    type = string  
+  description = "The path to the required cloud-init.yaml file for example './cloud-init.yaml'.  It is recommended to download and use https://github.com/mr-stringer/testwebserver/releases/download/v1.0.0/cloud-init.yaml"
+  type        = string
 }
 
 variable "vmCount" {
-    description = "The number of webserver VMs that will be built - defaults to 2"
-    type = number
-    default = 2  
+  description = "The number of webserver VMs that will be built - defaults to 2"
+  type        = number
+  default     = 2
 }
 
 variable "adminUser" {
-    description = "The name of the admin user account of the VMs, defaults to terry"
-    type = string
-    default = "terry"
+  description = "The name of the admin user account of the VMs, defaults to terry"
+  type        = string
+  default     = "terry"
 }
 
 #Required
 variable "adminUserPubKey" {
-    description = "The ssh public key of the admin user"
-    type = string
+  description = "The ssh public key of the admin user"
+  type        = string
 }
 
 locals {
-    vmName = "${var.projectName}-vm"
-    nicName = "${var.projectName}-nic"
-    nicIpConfName = "${var.projectName}-ipConf"
-    nsgName = "${var.projectName}-nsg"
-    natGwName = "${var.projectName}-natGw"
-    natGwPubIpName = "${var.projectName}-natGw-pubIp"
-    lbName = "${var.projectName}-lb"
-    lbPubIpName = "${var.projectName}-lb-pubIp"
-    lbFeIpCfgName = "${var.projectName}-lb-IpCfg"
+  vmName         = "${var.projectName}-vm"
+  nicName        = "${var.projectName}-nic"
+  nicIpConfName  = "${var.projectName}-ipConf"
+  nsgName        = "${var.projectName}-nsg"
+  natGwName      = "${var.projectName}-natGw"
+  natGwPubIpName = "${var.projectName}-natGw-pubIp"
+  lbName         = "${var.projectName}-lb"
+  lbPubIpName    = "${var.projectName}-lb-pubIp"
+  lbFeIpCfgName  = "${var.projectName}-lb-IpCfg"
 }
